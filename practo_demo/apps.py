03bc45825bd312721becfb67243468deae3b657b
VinayPep <vinaychauhan1011998@gmail.com>
2022-02-22 16:26:44 +0530

    App and Project start

---
+++ practo_demo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PractoDemoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'practo_demo'
---
+++ practo_demo/models.py
@@ -0,0 +1,6 @@
+from django.core import validators
+from django.db import models
+
+# Create your models here.
+
+
