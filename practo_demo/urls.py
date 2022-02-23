6dd0706a6551e69d3fc004ba00f3e90b3614b409
VinayPep <vinaychauhan1011998@gmail.com>
2022-02-23 15:23:32 +0530

    Add urls and views

--- practo_demo/urls.py
+++ practo_demo/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index)
+    path('user',views.UserAPI.as_view()),
+    path('user/<int:pk>',views.UserAPI.as_view()),
+    path('doctor/',views.DoctorAPI.as_view()),
+    path('doctor/<int:pk>',views.DoctorAPI.as_view())
+    
+
 ]
